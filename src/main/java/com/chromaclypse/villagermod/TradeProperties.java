4ed46a3261f873afa17d1613d4e461b2839fec3f
ezeiger92 <ezeiger92@gmail.com>
2020-01-11 23:46:54 -0500

    Value extractor cleanup

--- src/main/java/com/chromaclypse/villagermod/TradeProperties.java
+++ src/main/java/com/chromaclypse/villagermod/TradeProperties.java
@@ -1,42 +1,35 @@
 package com.chromaclypse.villagermod;
 
-import org.bukkit.entity.Villager;
+import com.chromaclypse.villagermod.VillagerTracker.Data;
 
 public class TradeProperties {
-	private Villager villager;
+	private final Data data;
 	
-	public TradeProperties(Villager villager) {
-		this.villager = villager;
+	public TradeProperties(Data data) {
+		this.data = data;
 	}
 	
-	private int getInt(String key, int limit) {
-		return (villager.getUniqueId() + key).hashCode() % limit; // Use tracked UUID instead
+	public int getInt(String key, int bound) {
+		return (data.getTrackedUUID() + key).hashCode() % bound;
 	}
 	
-	private int getVaryingInt(String key, int scale, int limit) {
-		return getInt(key + (villager.getTicksLived() / scale), limit);
+	public int getInt(String key, int bound, int minimum) {
+		return minimum + getInt(key, bound - minimum);
 	}
 	
-	private int getTimedInt(String key, int scale, int limit) {
-		return (villager.getTicksLived() / scale) % limit;
+	public int getRandomInt(String key, int tickRefresh, int bound) {
+		return getInt(key + (data.getTradeTimer() / tickRefresh), bound);
 	}
 	
-	private int getInt(String key, int lowerBase, int upperBase, int lowerRange, int upperRange, int timeScale) {
-		int base = getInt(key + "-base", upperBase - lowerBase) + lowerBase;
-		int range = getInt(key + "-range", upperRange - lowerRange) + lowerRange;
-		
-		return getVaryingInt(key, timeScale, range) + base;
+	public int getRandomInt(String key, int tickRefresh, int bound, int minimum) {
+		return minimum + getRandomInt(key, tickRefresh, bound - minimum);
 	}
 	
-	private boolean getBool(String key) {
-		return getBool(key, 2, 1);
+	public boolean getBool(String key) {
+		return getInt(key, 2) == 1;
 	}
 	
-	private boolean getBool(String key, int maximum, int required) {
-		return getInt(key, maximum) >= required;
+	public boolean getBool(String key, double probability) {
+		return getInt(key, 10000) / (double)10000 < probability;
 	}
-	//12e7e477-cb7b-455e-a998-650aa6d9b007
-	//e64d21fe-e720-411f-9d07-b4f1424e3af5
-	//e64d21fe-e720-411f-9d07-b4f1424e3af5
-	
 }
